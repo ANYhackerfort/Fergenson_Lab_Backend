2321fa11a904f0742da6b4b55fbfaa7560873598
ANYhackerfort <zhanglinghe610@gmail.com>
2024-11-19 20:33:05 -0800

    fixed

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Create and navigate to the build directory
+rm -rf build
 mkdir -p build
 cd build
+
+# cmake -DCMAKE_PREFIX_PATH=/Users/matthewzhang/websocket-Fergenson/libs/libtorch ..
 
 # Run CMake and Make
 cmake ..
